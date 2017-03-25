581a82a4c635891ff0a7ef9cecebf2a6e05ca87d
Aycan <aycandoganlar@outlook.com>
2017-03-25 12:44:08 +0300

    Django Girls uygulamam, ilk commit

---
+++ blogging/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+	yazar = models.ForeignKey('auth.User')
+	baslik = models.CharField(max_length=200)
+	yazi = models.TextField()
+	yaratilma_tarihi = models.DateTimeField(
+		default=timezone.now)
+	yayinlanma_tarihi = models.DateTimeField(
+		blank=True, null=True)
+
+	def yayinla(self):
+		self.yayinlanma_tarihi = timezone.now()
+		self.save()
+
+	def  __str__(self):
+		return self.baslik
+
+# Create your models here.
