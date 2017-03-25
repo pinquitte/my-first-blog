2506bfc4c6ae7fa530437b6f863d49b260b297dc
Aycan <aycandoganlar@outlook.com>
2017-03-25 16:40:28 +0300

    Veritabanındaki dosyaları görebilmek için template düzenlendi.

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):     
-    return render(request, 'blogging/post_list.html', {})
+    posts = Post.objects.filter(yayinlanma_tarihi__lte=timezone.now()).order_by('yayinlanma_tarihi')
+    return render(request, 'blogging/post_list.html', {'posts':posts})
 
 # Create your views here.
